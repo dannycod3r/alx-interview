ebd6d78d48af11725ec7ede941fe2c1068587a74
dannycod3r <oseid7807@gmail.com>
2024-07-27 08:22:53 +0000

    Locked boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """function that determines if all boxes are opened"""
+    n = len(boxes)
+    opened = [False] * n  # track opened boxes
+    opened[0] = True  # first box is already opened
+    keys = [0]  # begin with first box
+
+    while keys:
+        current = keys.pop()
+        for key in boxes[current]:
+            if key < n and not opened[key]:
+                opened[key] = True
+                keys.append(key)
+
+    return all(opened)  # check if all boxes are opend
