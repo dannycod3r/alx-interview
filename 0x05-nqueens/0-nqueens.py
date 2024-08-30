5155a97bb92c56f968bbacc041d8bee9a20dffeb
dannycod3r <oseid7807@gmail.com>
2024-08-30 04:08:09 +0000

    Add program that find N-queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""A script that prints statistics of intercepted log files
+"""
+import sys
+
+
+def is_safe(board, row, col, N):
+    # Check this row on the left side
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    # Check upper diagonal on the left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check lower diagonal on the left side
+    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens_util(board, col, N, solutions):
+    if col >= N:
+        # A solution is found, store it in the solutions list
+        solution = []
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        solutions.append(solution)
+        return True
+
+    res = False
+    for i in range(N):
+        if is_safe(board, i, col, N):
+            board[i][col] = 1  # Place the queen
+            # Recur to place the rest of the queens
+            res = solve_nqueens_util(board, col + 1, N, solutions) or res
+            board[i][col] = 0  # Backtrack
+
+    return res
+
+
+def solve_nqueens(N):
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    solutions = []
+    solve_nqueens_util(board, 0, N, solutions)
+    return solutions
+
+
+def print_solutions(solutions):
+    for solution in solutions:
+        print(solution)
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solutions = solve_nqueens(N)
+    print_solutions(solutions)
+
+
+if __name__ == "__main__":
+    main()
